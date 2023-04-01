48c29a3711ec6f4342edb83987dfdfd41bbf35e2
Junaidhkn <junaidhkniazi@gmail.com>
2023-04-02 00:12:36 +0500

    tailwind added

---
+++ app/head.tsx
@@ -0,0 +1,22 @@
+const Head = () => {
+	return (
+		<>
+			<title>My page title</title>
+			<meta
+				property='og:title'
+				content='My page title'
+				key='title'
+			/>
+
+			<meta
+				property='og:title'
+				content='My new title'
+				key='title'
+			/>
+
+			<p>Hello world!</p>
+		</>
+	);
+};
+
+export default Head;
