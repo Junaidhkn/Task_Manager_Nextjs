203f288bb73a1eec7cd3e2e6cbc96fcb8ed146ac
Junaidhkn <junaidhkniazi@gmail.com>
2023-04-25 21:20:41 +0500

    commit

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const fetcher = async ({ url, method, body, json = true }) => {
+export const fetcher = async ({ url, method, body, json = true }) => {
 	const res = await fetch(url, {
 		method,
 		...(body && { body: JSON.stringify(body) }),
@@ -9,27 +9,29 @@ const fetcher = async ({ url, method, body, json = true }) => {
 	});
 
 	if (!res.ok) {
-		throw new Error('API Error');
+		// handle your errors
+		throw new Error('API error');
 	}
 
 	if (json) {
 		const data = await res.json();
-		return data;
+		return data.data;
 	}
 };
 
-export const register = async (user) => {
-	return fetcher({
-		url: '/api/register',
-		method: 'POST',
-		body: user,
-	});
+export const register = (user) => {
+	return fetcher({ url: '/api/register', method: 'post', body: user });
 };
 
-export const signin = async (user) => {
+export const signin = (user) => {
+	return fetcher({ url: '/api/signin', method: 'post', body: user });
+};
+
+export const createNewProject = async (name) => {
 	return fetcher({
-		url: '/api/signin',
+		url: '/api/project',
 		method: 'POST',
-		body: user,
+		body: { name },
+		json: true,
 	});
 };
